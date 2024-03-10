72e2c3070b916bfd36c3aad7d50445e80ff755a9
Teelo <49792789+teelolws@users.noreply.github.com>
2024-03-11 11:39:14 +1300

    Create GoblinEngineering.lua

---
+++ Source/db/vanilla/GoblinEngineering.lua
@@ -0,0 +1,72 @@
+local addonName, addon = ...
+local sources = addon.Enums.Sources
+
+addon.db.GoblinEngineering = {
+	[12715] = {
+		spellID = 12715,
+		minSkill = 205,
+		source = sources.Trainer,
+	},
+	[12760] = {
+		spellID = 12760,
+		minSkill = 205,
+		source = sources.Trainer,
+	},
+	[12717] = {
+		spellID = 12717,
+		minSkill = 205,
+		source = sources.Trainer,
+	},
+	[12718] = {
+		spellID = 12718,
+		minSkill = 205,
+		source = sources.Trainer,
+	},
+	[13240] = {
+		spellID = 13240,
+		minSkill = 205,
+		source = sources.Trainer,
+	},
+	[15628] = {
+		spellID = 15628,
+		minSkill = 205,
+		source = sources.Item,
+		itemID = 11828,
+		itemSource = 'Goblin Engineer\'s Renewal Gift',
+	},
+	[12716] = {
+		spellID = 12716,
+		minSkill = 225,
+		source = sources.Unknown,
+	},
+	[8895] = {
+		spellID = 8895,
+		minSkill = 225,
+		source = sources.Trainer,
+	},
+	[12755] = {
+		spellID = 12755,
+		minSkill = 230,
+		source = sources.Trainer,
+	},
+	[12754] = {
+		spellID = 12754,
+		minSkill = 235,
+		source = sources.Trainer,
+	},
+	[12908] = {
+		spellID = 12908,
+		minSkill = 240,
+		source = sources.Trainer,
+	},
+	[12758] = {
+		spellID = 12758,
+		minSkill = 245,
+		source = sources.Trainer,
+	},
+	[23486] = {
+		spellID = 23486,
+		minSkill = 285,
+		source = sources.Unknown,
+	},
+}
