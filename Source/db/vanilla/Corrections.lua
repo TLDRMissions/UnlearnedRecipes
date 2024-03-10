27028115724f3b377421b1249c06707b43bc6ba1
Teelo <49792789+teelolws@users.noreply.github.com>
2024-03-11 11:45:48 +1300

    Update Corrections.lua

--- Source/db/vanilla/Corrections.lua
+++ Source/db/vanilla/Corrections.lua
@@ -13,3 +13,17 @@ addon.db.Tailoring[12044] = nil
 -- Tailoring version of Gordok Ogre Suit, comes from a quest in Dire Maul
 addon.db.Tailoring[22813].source = sources.Quest
 addon.db.Tailoring[22813].sourceQuestID = 5518
+
+-- Merge the relevant Engineering specialisation recipes into Engineering
+if C_QuestLog.IsQuestFlaggedCompleted(3639) then
+    for k, v in pairs(addon.db.GoblinEngineering) do
+        addon.db.Engineering[k] = v
+    end
+end
+if C_QuestLog.IsQuestFlaggedCompleted(3643) then
+    for k, v in pairs(addon.db.GnomishEngineering) do
+        addon.db.Engineering[k] = v
+    end
+end
+addon.db.GoblinEngineering = nil
+addon.db.GnomishEngineering = nil
