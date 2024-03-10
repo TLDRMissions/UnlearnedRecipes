3233c853a0a44d8b98a7a92c55642f24220d82d5
Teelo <49792789+teelolws@users.noreply.github.com>
2024-03-11 11:39:17 +1300

    Create Smelting.lua

---
+++ Source/db/vanilla/Smelting.lua
@@ -0,0 +1,65 @@
+local addonName, addon = ...
+local sources = addon.Enums.Sources
+
+addon.db.Smelting = {
+	[2657] = {
+		spellID = 2657,
+		minSkill = 25,
+		source = sources.Unknown,
+	},
+	[3304] = {
+		spellID = 3304,
+		minSkill = 65,
+		source = sources.Trainer,
+	},
+	[2659] = {
+		spellID = 2659,
+		minSkill = 65,
+		source = sources.Trainer,
+	},
+	[2658] = {
+		spellID = 2658,
+		minSkill = 75,
+		source = sources.Trainer,
+	},
+	[3307] = {
+		spellID = 3307,
+		minSkill = 125,
+		source = sources.Trainer,
+	},
+	[3308] = {
+		spellID = 3308,
+		minSkill = 155,
+		source = sources.Trainer,
+	},
+	[3569] = {
+		spellID = 3569,
+		minSkill = 165,
+		source = sources.Trainer,
+	},
+	[10097] = {
+		spellID = 10097,
+		minSkill = 175,
+		source = sources.Trainer,
+	},
+	[10098] = {
+		spellID = 10098,
+		minSkill = 230,
+		source = sources.Unknown,
+	},
+	[14891] = {
+		spellID = 14891,
+		minSkill = 230,
+		source = sources.Unknown,
+	},
+	[16153] = {
+		spellID = 16153,
+		minSkill = 250,
+		source = sources.Trainer,
+	},
+	[22967] = {
+		spellID = 22967,
+		minSkill = 310,
+		source = sources.Unknown,
+	},
+}
