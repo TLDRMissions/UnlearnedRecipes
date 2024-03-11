9d2ac7568b2e9e77edb203750b7f3dff8bdc4edd
Teelo <49792789+teelolws@users.noreply.github.com>
2024-03-11 14:20:25 +1300

    Update enums.lua

--- Source/enums.lua
+++ Source/enums.lua
@@ -4,13 +4,25 @@ addon.Enums = {}
 addon.Strings = {}
 
 addon.Enums.Professions = {}
-addon.Enums.Professions.Tailoring = 1
+addon.Enums.Professions.Alchemy = 1
+addon.Enums.Professions.Blacksmithing = 2
+addon.Enums.Professions.Enchanting = 3
+addon.Enums.Professions.Engineering = 4
+addon.Enums.Professions.Leatherworking = 5
+addon.Enums.Professions.Smelting = 6
+addon.Enums.Professions.Tailoring = 7
 
 addon.Enums.Reacts = {}
 addon.Enums.Reacts.Horde = 1
 addon.Enums.Reacts.Ally = 2
 
 addon.Strings.Professions = {}
+addon.Strings.Professions.Alchemy = GetSpellInfo(2259)
+addon.Strings.Professions.Blacksmithing = GetSpellInfo(2018)
+addon.Strings.Professions.Enchanting = GetSpellInfo(7411)
+addon.Strings.Professions.Engineering = GetSpellInfo(4036)
+addon.Strings.Professions.Leatherworking = GetSpellInfo(2108)
+addon.Strings.Professions.Smelting = GetSpellInfo(2656)
 addon.Strings.Professions.Tailoring = GetSpellInfo(3909)
 
 addon.Enums.Sources = {}
