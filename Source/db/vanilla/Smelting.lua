495af9a3eec9f97bcb98cef18a68965afa3d92c0
Teelo <49792789+teelolws@users.noreply.github.com>
2024-03-11 15:14:22 +1300

    Add resulting item ID to databases

--- Source/db/vanilla/Smelting.lua
+++ Source/db/vanilla/Smelting.lua
@@ -5,61 +5,73 @@ addon.db.Smelting = {
 	[2657] = {
 		spellID = 2657,
 		minSkill = 25,
+		itemID = 2840,
 		source = sources.Unknown,
 	},
 	[3304] = {
 		spellID = 3304,
 		minSkill = 65,
+		itemID = 3576,
 		source = sources.Trainer,
 	},
 	[2659] = {
 		spellID = 2659,
 		minSkill = 65,
+		itemID = 2841,
 		source = sources.Trainer,
 	},
 	[2658] = {
 		spellID = 2658,
 		minSkill = 75,
+		itemID = 2842,
 		source = sources.Trainer,
 	},
 	[3307] = {
 		spellID = 3307,
 		minSkill = 125,
+		itemID = 3575,
 		source = sources.Trainer,
 	},
 	[3308] = {
 		spellID = 3308,
 		minSkill = 155,
+		itemID = 3577,
 		source = sources.Trainer,
 	},
 	[3569] = {
 		spellID = 3569,
 		minSkill = 165,
+		itemID = 3859,
 		source = sources.Trainer,
 	},
 	[10097] = {
 		spellID = 10097,
 		minSkill = 175,
+		itemID = 3860,
 		source = sources.Trainer,
 	},
 	[10098] = {
 		spellID = 10098,
 		minSkill = 230,
+		itemID = 6037,
 		source = sources.Unknown,
 	},
 	[14891] = {
 		spellID = 14891,
 		minSkill = 230,
+		itemID = 11371,
 		source = sources.Unknown,
 	},
 	[16153] = {
 		spellID = 16153,
 		minSkill = 250,
+		itemID = 12359,
 		source = sources.Trainer,
 	},
 	[22967] = {
 		spellID = 22967,
 		minSkill = 310,
+		itemID = 17771,
 		source = sources.Unknown,
 	},
 }
